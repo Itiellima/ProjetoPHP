7313261311a42f11cc9bec14f3ad757ee7d798f3
Itiellima <itiel.lima.cavalcante@gmail.com>
2025-07-17 00:20:10 -0300

    Link ionicons

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -50,5 +50,9 @@
         <footer>
             <p>Projeto1 &copy; 2025</p>
         </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        
     </body>
 </html>
