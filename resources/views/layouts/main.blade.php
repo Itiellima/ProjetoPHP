0e35101b95c294f9556cc3652adf539f6230cc78
Itiellima <itiel.lima.cavalcante@gmail.com>
2025-07-20 14:55:00 -0300

    flash message

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,7 +45,17 @@
             </nav>
         </header>
 
-        @yield('content')
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if (session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>    
+                    @endif
+
+                    @yield('content')
+                </div>
+            </div>
+        </main>
 
         <footer>
             <p>RN &copy; 2025</p>
