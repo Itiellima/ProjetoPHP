c594724a5a9b0854788ad6063ef55b438bb87538
Itiellima <itiel.lima.cavalcante@gmail.com>
2025-07-23 12:45:40 -0300

    Busca por paramentro de pesquisa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,21 @@
 
     <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-        <form action="">
+        <form action="/" method="GET">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
         </form>
     </div>
 
     
     <div id="events-container" class="col-md-12">
+        @if ($search)
+        <h2>Buscando por: {{ $search }}</h2>
+        @else
         <h2 class="subtitle">Proximos eventos</h2>
         <p>Veja os eventos dos proximos dias</p>
+        @endif
+
+        
         <div id="cards-conteiners" class="row">
             @foreach ($events as $event)
             <div class="card" style="width: 20%;">
@@ -36,7 +42,9 @@
             </div>
 
             @endforeach
-            @if (count($events) == 0)
+            @if (count($events) == 0 && $search)
+                <p>Não foi possivel encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
+            @elseif(count($events) == 0)
                 <p>Nao há eventos disponiveis</p>
             @endif
         </div>
